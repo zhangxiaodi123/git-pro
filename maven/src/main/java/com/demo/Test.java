60583eebfdf31912718904ae35b59f2e880c53c8
zhangxiaodi123 <xiaodi_97@163,com>
2020-03-23 17:06:04 +0800

    第一次上传

---
+++ maven/src/main/java/com/demo/Test.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Mrα张
+ * @Date: 2020/03/23/17:04:27
+ * @Description:
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
